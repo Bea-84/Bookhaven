f03742bd247659a40fd446c2806f1602ae3d9b8f
Bea-84 <bea_erlin93@alumnes.ilernabcn.com>
2023-12-18 12:12:14 +0100

    Conexión desde Ilerna o desde casa

--- config/parameters.php
+++ config/parameters.php
@@ -1,7 +1,8 @@
 <?php
 
-
+//Conexión desde Ilerna
 define("url","http://localhost/Bookhaven/");
+//Conexión desde casa
+//define("url","http://localhost:8080/Bookhaven/");
 define("action_default","list");
 
-//hola
